9f8924abc689f89b628cb76db649563b2e296e91
RAMON MULLER <ramon_muller1@estudante.sesisenai.org.br>
2025-03-05 11:02:07 -0300

    Implementação das mecanicas de personagem

--- script.js
+++ script.js
@@ -2,47 +2,51 @@
 //inicializar o canvas
 const canvas = document.getElementById('jogo2D');
 const ctx =  canvas.getContext('2d');
-
+const gravidade = 0.5;
 
 const personagem = {
     x: 100,
     y: canvas.height - 50,
     altura: 50,
     largura: 50,
-    cor: 'white',
     velocidadeY: 0,
-    pulando: false,
+    pulando: false
 }
 
 document.addEventListener('keypress', (e) =>{
-    if(e.code == 'Space'){
-        console.log("Clicou para pular");
-        personagem.velocidadeY = 15;
-        personagem.pulando = true;
+    if(e.code == 'Space' && personagem.y == (canvas.height - 50)) {
+            console.log("Clicou para pular");
+            personagem.velocidadeY = 15
+            personagem.pulando = true
     }
 })
 
 
 const desenharPersonagem = () =>{
     ctx.fillStyle = 'white'
-    ctx.fillRect( personagem.x, (canvas.height - personagem.altura), personagem.largura, personagem.altura);
+    ctx.fillRect( personagem.x, personagem.y, personagem.largura, personagem.altura);
 }
 
 
 const atualizarPersonagem = () => {
     /**Etapa do pulo:
-      1: Capturar evento
-      2: Aumentar velocidade y
-      3: Mexer na função de atualizar o personagem
-     */
+    1: Capturar evento
+    2: Aumentar velocidade y
+    3: Mexer na função de atualizar o personagem*/
     
-    if(personagem.pulando == true){
-        personagem.y -= personagem.velocidadeY
+    if(personagem.pulando === true){        
+        personagem.velocidadeY -= gravidade;
+        personagem.y -= personagem.velocidadeY;
+        if(personagem.y >= (canvas.height - 50)){
+            personagem.velocidadeY = 0
+            personagem.pulando = false
+            personagem.y = (canvas.height - 50)
+        }
     }
 }
 
-//enviar função loop. querepetirá 60 vezes por segundo
-//(60Hz)
+/*enviar função loop. querepetirá 60 vezes por segundo
+(60Hz)*/
 
 /**Criar função loop */
 const loop = () => {
@@ -58,5 +62,4 @@ const loop = () => {
     /*Chamar loop denovo */
     requestAnimationFrame(loop)
 }
-
 loop();
