879fda1e2d58fdf59209e0b4ccc60c844dc0c0a9
RAMON MULLER <ramon_muller1@estudante.sesisenai.org.br>
2025-03-05 09:19:51 -0300

    Creating base functions

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-a = 1,
-b = 2
+//Pegar elemento canvas pelo id
+const canvas = document.getElementById('jogo2D');
+//inicializar o canvas
+const ctx =  canvas.getContext('2d');
+
+//enviar função loop. querepetirá 60 vezes por segundo
+//(60Hz)
+
+const loop = () => {
+    
+}
+
+loop();
