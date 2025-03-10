826a7e6ad489a9d92f4b61f6ed45053e7c88a43b
RAMON MULLER <ramon_muller1@estudante.sesisenai.org.br>
2025-03-10 11:13:27 -0300

    Orientação a objetos e classes

---
+++ orientacao_Objects/object+class.js
@@ -0,0 +1,35 @@
+class vehicle {
+    constructor(tipo, marca, cor, velocidade, passageiros) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.cor = cor;
+        this.velocidade = velocidade;
+        this.passageiros = passageiros;
+    }
+    acelerar = function () {
+        this.velocidade += 10;
+        console.log(this.velocidade)
+    }
+    freiar = () => {
+        if(this.velocidade > 0) {
+            this.velocidade -=10;
+            console.log(this.velocidade)
+        }
+    }
+}
+
+
+const criacao = () => {
+    const carro = new vehicle('sedan', 'Mercedes', 'preto', 0, 5)
+    const outro_carro = new vehicle('wagon', 'Volkswagen', 'azul petroleo', 0 ,5)
+    console.log(carro);
+    console.log(outro_carro);
+    outro_carro.acelerar();
+    carro.acelerar();
+    carro.acelerar();
+    carro.freiar();
+    carro.freiar();
+    outro_carro.freiar();
+
+}
+criacao();
