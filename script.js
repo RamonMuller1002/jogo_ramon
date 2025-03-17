db70f28b3b14b369612f472f7978904c506d8513
RAMON MULLER <ramon_muller1@estudante.sesisenai.org.br>
2025-03-17 11:06:53 -0300

    Adicionando imagens, multi-obstaculos e pontuação

--- script.js
+++ script.js
@@ -1,125 +1,161 @@
-//Pegar elemento canvas pelo id
-//inicializar o canvas
+// Pegar elemento canvas pelo id
+// Inicializar o canvas
 const canvas = document.getElementById('jogo2D');
 const ctx = canvas.getContext('2d');
 let game_Over = false;
+let pontuacao = document.getElementById('pontuacao').innerHTML
 
-class Entidade{
-    #gravidade
-    constructor(x, y, largura, altura){
+class Entidade {
+    #gravidade;
+    constructor(x, y, largura, altura) {
         this.x = x;
         this.y = y;
         this.largura = largura;
         this.altura = altura;
         this.#gravidade = 0.5;
     }
+
     desenharEntidade = function (ctx, cor) {
-        ctx.fillStyle = cor
+        ctx.fillStyle = cor;
         ctx.fillRect(this.x, this.y, this.largura, this.altura);
-    }
-    //padronizado pela convenção do JS
+    };
+
+    // 'Getter' Padronizado pela convenção do JS
     get gravidade() {
-        return this.#gravidade
+        return this.#gravidade;
     }
 }
 
-class Personagem extends Entidade{
-    #pulando
-    #velocidadeY
-    constructor(x, y, largura, altura){
+class Personagem extends Entidade {
+    #pulando;
+    #velocidadeY;
+    constructor(x, y, largura, altura) {
         super(x, y, largura, altura);
         this.#pulando = false;
         this.#velocidadeY = 0;
-        //this.img_src = './static/Dino.png';
-        //this.imagem = new Image(50, 50);
+        this.img_src = 'static/Dino.png';
+        this.imagem = new Image();
+        this.imagem.src = this.img_src;
     }
-    saltar = function (){
-        personagem.#velocidadeY = 17
-        personagem.#pulando = true;
-        console.log('pulou')
-    }
-    get pulando(){
+    desenharEntidade = function (ctx) {
+        ctx.drawImage(this.imagem, this.x, this.y, this.largura, this.altura);
+    };
+    
+    saltar = function () {
+        this.#velocidadeY = 17;
+        this.#pulando = true;
+    };
+
+    get pulando() {
         return this.#pulando;
     }
 
     atualizarPersonagem = () => {
-        /**Etapa do pulo:
-        1: Capturar evento
-        2: Aumentar velocidade y
-        3: Mexer na função de atualizar o personagem*/
-    
+        /** Etapa do pulo:
+         1: Capturar evento
+         2: Aumentar velocidade y
+         3: Mexer na função de atualizar o personagem*/
+
         if (this.#pulando === true) {
             this.#velocidadeY -= this.gravidade;
             this.y -= this.#velocidadeY;
-            if (this.y >= (canvas.height - 50)) {
-                this.#velocidadeY = 0
-                this.#pulando = false
-                this.y = (canvas.height - 50)
+            if (this.y >= canvas.height - 50) {
+                this.#velocidadeY = 0;
+                this.#pulando = false;
+                this.y = canvas.height - 50;
             }
         }
-    }
-    colisao = () => {
-        if (personagem.x < obstaculo.x + obstaculo.largura && personagem.x + personagem.largura > obstaculo.x &&
-            personagem.y < obstaculo.y + obstaculo.altura && personagem.y + personagem.altura > obstaculo.y) {
-            gameOver();
-        }
-    }
-}
+    };
 
-class Obstaculo extends Entidade{
-    #velocidadeX
-    constructor(x, y, largura, altura, img_src, imagem ){
-        super(x, y, largura, altura, img_src, imagem );
+    colisao = (obstaculo) => {
+            if (this.x < obstaculo.x + obstaculo.largura &&
+                this.x + this.largura > obstaculo.x &&
+                this.y < obstaculo.y + obstaculo.altura &&
+                this.y + this.altura > obstaculo.y) {
+                gameOver();
+            }
+        };
+    };
+
+class Obstaculo extends Entidade {
+    #velocidadeX;
+    #time_distancia;
+    #proximo_obstaculo
+    constructor(x, y, largura, altura) {
+        super(x, y, largura, altura);
         this.#velocidadeX = 5;
-        //this.img_src = './static/Pinheiro.png';
-        //this.imagem = new Image(50, 100);
+        this.#time_distancia = Math.floor((Math.random() * 100) + 100)
+        console.log(this.#time_distancia)
+        this.#proximo_obstaculo = false
+        this.img_src = 'static/Arvore.png';
+        this.imagem = new Image();
+        this.imagem.src = this.img_src;
+    }
 
-    }
-    
-    atulalizaObstaculo = () => {
-        obstaculo.x -= this.#velocidadeX
-        if (obstaculo.x <= 0 - obstaculo.largura) {
-                obstaculo.x = canvas.width
-                this.#velocidadeX += 0.2;
-                let novaAltura = (Math.random() * 50) + 100
-                obstaculo.altura += novaAltura
-                obstaculo.y = canvas.height - novaAltura
+    atualizarObstaculo = () => {
+        this.x -= this.#velocidadeX;
+        if (this.#time_distancia <= 0 && this.#proximo_obstaculo == false){
+            let nova_altura = (Math.random() * 50) + 100
+            let novoY = canvas.height - nova_altura 
+            obstaculos.push(new Obstaculo(canvas.width-100,novoY,50,nova_altura))
+            this.#velocidadeX += 0.2
+            this.#proximo_obstaculo = true
+        } else{
+            this.#time_distancia--
         }
-    }
+        if (this.x <= 0-this.largura && this.#proximo_obstaculo == true){
+            //deleta o elemento de um obstaculo no array
+            obstaculos.shift()
+        }
+    };  
+
+    desenharEntidade = function (ctx) {
+        ctx.drawImage(this.imagem, this.x, this.y, this.largura, this.altura);
+    };
 }
 
 document.addEventListener('keypress', (e) => {
     if (e.code == 'Space' && personagem.pulando == false) {
         personagem.saltar();
     }
-})
+});
 
 document.addEventListener('click', (e) => {
-    if(game_Over == true){
+    if (game_Over == true) {
         location.reload();
     }
-})
+});
+
+
+tempoDeInicio = Date.now();
+
+function printPontuacao(pontuacao){
+    if (tempoDeInicio) {
+        pontuacao = Math.floor((Date.now() - tempoDeInicio) / 1000);
+        document.getElementById('pontuacao').innerHTML = pontuacao;
+    }
+}
 
 function gameOver() {
-    game_Over = true
+    game_Over = true;
+    personagem.velocidadeY = 0;
 
-    personagem.velocidadeY = 0
-    obstaculo.velocidadeX = 0
-    
-    ctx.fillStyle = 'RED'
-    ctx.fillRect((canvas.width / 2) - 200, (canvas.height / 2) -50 , 400 , 100)
+    ctx.fillStyle = 'RED';
+    ctx.fillRect((canvas.width / 2) - 200, (canvas.height / 2) - 50, 400, 100);
 
-    ctx.fillStyle = 'black'
-    ctx.font = "50px Arial"
-    ctx.fillText("Game Over",(canvas.width / 2) - 150, canvas.height / 2)
-    
+    ctx.fillStyle = 'black';
+    ctx.font = "50px Arial";
+    ctx.fillText("Game Over", (canvas.width / 2) - 150, canvas.height / 2);
+
 }
 
 
-const personagem = new Personagem(50, canvas.height - 50, 50 ,50);
-const obstaculo = new Obstaculo(canvas.width - 100, canvas.height - 50, 50 ,150);
+const personagem = new Personagem(50, canvas.height - 50, 50, 50);
+const obtaculo = new Obstaculo(canvas.width - 100, canvas.height - 50, 50 ,150);
+const obstaculos = []
+obstaculos.push(new Obstaculo(canvas.width-100,canvas.height-100,50,100))
 
-/*Criar função loop /enviar função loop. querepetirá 60 vezes por segundo(60Hz) */
+/* Criar função loop /enviar função loop. que repetirá 60 vezes por segundo (60Hz) */
 const loop = () => {
     if (game_Over == false) {
         /*Apagar a tela anterior */
@@ -128,13 +164,15 @@ const loop = () => {
     
         //Desenhar novamente
         personagem.desenharEntidade(ctx, 'black');
-        obstaculo.desenharEntidade(ctx, 'RED');
+        obstaculos.forEach((obstaculo) =>{
+            obstaculo.desenharEntidade(ctx, 'red');
+            personagem.colisao(obstaculo);
+            obstaculo.atualizarObstaculo();
+        })
 
-        personagem.colisao();
-
+        printPontuacao(pontuacao);
         //Atualizar posições 
         personagem.atualizarPersonagem();
-        obstaculo.atulalizaObstaculo();
 
         //Chamar loop denovo 
         requestAnimationFrame(loop)
